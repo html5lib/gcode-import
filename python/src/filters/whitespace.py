cbd665d9fe902a4f3fa32f9ca10b2ddb06a4060f
t.broyer <t.broyer@localhost>
2007-06-13 20:15:11 +0000

    Added an "inject <meta charset>" filter and plugged it into the serializer.

--- python/src/filters/whitespace.py
+++ python/src/filters/whitespace.py
@@ -24,26 +24,32 @@ class Filter(_base.Filter):
     def _getName(self):
         if preserve or self.source.name in self.spacePreserveElements:
             preserve += 1
-    name = property(_getName, doc=_base.Filter.name)
+    name = property(_getName, doc=_base.Filter.name.__doc__)
 
     def _getValue(self):
         text = self.source.value
         if self.type == "Text":
+            # if text nodes haven't been collapsed, we leave leading and
+            # trailing spaces to ensure words from other adjacent text
+            # nodes won't get concatenated...
             if self.collapsed_text:
+                # ...otherwise, we can strip leading and trailing spaces.
                 text = text.strip(spaceCharacters)
             text = collapse_spaces(text)
         return text
-    value = property(_getValue, doc=_base.Filter.value)
+    value = property(_getValue, doc=_base.Filter.value.__doc__)
 
     def firstChild(self):
         if preserve:
             preserve += 1
         self.source.firstChild(self)
+    firstChild.__doc__ = _base.Filter.firstChild.__doc__
 
     def parentNode(self):
         if preserve:
             preserve -= 1
         self.source.parentNode(self)
+    parentNode.__doc__ = _base.Filter.parentNode.__doc__
 
 def collapse_spaces(text):
     return re.compile(u"[%s]+" % spaceCharacters).sub(' ', text)
