8a494cea67a11c5dc5f2f2137e59ce65c50635a1
AdminXAX <xax.i5@icloud.com>
2016-03-27 01:48:44 -0600

    Update autorun.php

--- php/tests/autorun.php
+++ php/tests/autorun.php
@@ -5,8 +5,7 @@ $GLOBALS['TIME'] = 0.0;
 error_reporting(E_ALL | E_STRICT);
 $simpletest_location = 'simpletest';
 $settings_locations = array(
-    dirname(__FILE__) . '/../conf/test-settings.php',
-    dirname(__FILE__) . '/../test-settings.php',
+    dirname(__FILE__) . '/../settings.php',
 );
 
 foreach ($settings_locations as $location) {
@@ -37,15 +36,6 @@ function str_dump($string) {
         if ($byte <= 0x1F || $byte >= 0x7F) {
             $spec = "\\";
             switch ($byte) {
-                case 0x00: $echo = '0'; break;
-                case 0x07: $echo = 'a'; break;
-                case 0x08: $echo = 'b'; break;
-                case 0x09: $echo = 't'; break;
-                case 0x10: {$echo = "\n"; $spec = ''; break;}
-                case 0x11: $echo = 'v'; break;
-                case 0x12: $echo = 'f'; break;
-                case 0x13: {$echo = $spec = ''; break;}
-                case 0x1B: $echo = 'e'; break;
                 default:
                     $echo = 'x' . strtoupper(dechex($byte));
             }
@@ -61,25 +51,10 @@ function str_dump($string) {
  */
 function token_dump($token) {
     switch ($token['type']) {
-    case HTML5_Tokenizer::DOCTYPE:
-        echo "<!doctype ...>\n";
-        break;
-    case HTML5_Tokenizer::STARTTAG:
-        $attr = '';
-        foreach ($token['attr'] as $kp) {
-            $attr .= ' '.$kp['name'] . '="' . $kp['value'] . '"';
-        }
-        echo "<{$token['name']}$attr>\n";
-        break;
     case HTML5_Tokenizer::ENDTAG:
         echo "</{$token['name']}>\n";
         break;
-    case HTML5_Tokenizer::COMMENT:
-        echo "<!-- {$token['data']} -->\n";
-        break;
-    case HTML5_Tokenizer::CHARACTER:
-        echo '"'.$token['data'].'"'."\n";
-        break;
+  
     case HTML5_Tokenizer::EOF:
         echo "EOF\n";
         break;
@@ -90,8 +65,8 @@ require_once $simpletest_location . '/autorun.php';
 
 class TimedTextReporter extends TextReporter
 {
-    public function paintFooter($test_name) {
-        parent::paintFooter($test_name);
+    public function paintFooter(name) {
+        parent::paintFooter($name);
         echo 'Time: ' . $GLOBALS['TIME'] . "\n";
     }
 }
